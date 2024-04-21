f03e312d765c0f37c337a984d1297b85d4a437a9
isabellagoes <isabellagoes.igs@gmail.com>
2024-04-21 14:41:26 -0300

    relembrando csharp

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite o numerador: ");
+double numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o denominador: ");
+double denominador = Convert.ToDouble(Console.ReadLine());
+
+if (denominador == 0)
+{
+    Console.Write("Não é possível dividir por zero.");
+}
+
+else
+{
+    double quociente = numerador / denominador;
+    Console.Write($"\n{numerador:N1} dividido por {denominador:N1} é {quociente:N1}.");
+}
